8bf7d4d4902ae8334555aa6c87607b1de69cbdb2
eunsik-kim <gracely9901@gmail.com>
2024-10-30 01:08:39 +0900

    refactor: string parser 구현 및 단위 테스트 완료

---
+++ src/main/java/calculator/model/CalculatorParser.java
@@ -0,0 +1,52 @@
+package calculator.model;
+
+import java.sql.Array;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CalculatorParser {
+    public String[] parseInput(String inputStr) {
+        if (inputStr == null) {
+            throw new IllegalArgumentException("Input cannot be null");
+        }
+
+        String delimiters = ",|:"; // 기본 구분자는 , or :
+        String numberStr = inputStr;
+
+        if (inputStr.startsWith("//")) {
+            CustomDelimiterResult result = parseCustomDelimiter(inputStr);
+            delimiters = String.format("%s|%s", delimiters, Pattern.quote(result.delimiter));
+            numberStr = result.numbers;
+        }
+
+        return splitWithDelimiter(numberStr, delimiters);
+    }
+    private CustomDelimiterResult parseCustomDelimiter(String inputStr) {
+        //  Custom 구분자는 // 와 \n 사이에 존재
+        Pattern pattern = Pattern.compile("//(.*?)\\\\n(.*)");
+        Matcher matcher = pattern.matcher(inputStr);
+
+        if (!matcher.find()) {
+            throw new IllegalArgumentException("Invalid custom delimiter format");
+        }
+
+        return new CustomDelimiterResult(
+                matcher.group(1),  // delimiter
+                matcher.group(2)   // numbers
+        );
+    }
+
+    private String[] splitWithDelimiter(String inputStr, String delimiters) {
+        return inputStr.split(delimiters);
+    }
+
+    private static class CustomDelimiterResult {
+        private final String delimiter;
+        private final String numbers;
+
+        public CustomDelimiterResult(String delimiter, String numbers) {
+            this.delimiter = delimiter;
+            this.numbers = numbers;
+        }
+    }
+}
---
+++ src/test/java/calculator/model/CalculatorParserTest.java
@@ -0,0 +1,69 @@
+package calculator.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.as;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CalculatorParserTest {
+    private CalculatorParser calculatorParser;
+
+    @BeforeEach
+    void setUp() {
+        this.calculatorParser = new CalculatorParser();
+    }
+
+    @ParameterizedTest
+    @DisplayName("입력 string을 BaseDelimitor로 parsing하여 string 배열로 만든다")
+    @CsvSource({
+            "'1,2,3,4,5'",
+            "'1,2,3:4:5'"
+    })
+    void parseStringWithBaseDelimitor(String input) {
+        // when
+        String[] result = calculatorParser.parseInput(input);
+
+        // then
+        assertThat(result).containsExactly("1", "2", "3", "4", "5");
+    }
+
+
+    @ParameterizedTest
+    @DisplayName("입력 string을 CustomDelimitor로 parsing하여 string 배열로 만든다")
+    @CsvSource(value = {
+            "'//.\\n1,2,3,4:5'",
+            "'//!\\n1!2!3,4:5'"
+    })
+    void parseStringWithCustomDelimitor(String input) {
+        // when
+        String[] result = calculatorParser.parseInput(input);
+
+        // then
+        assertThat(result).containsExactly("1", "2", "3", "4", "5");
+    }
+
+    @ParameterizedTest
+    @DisplayName("빈 string을 입력받는 경우 빈 array를 반환하는지 확인")
+    @ValueSource(strings = {"", "//!\\n"})
+    void parseEmptyString(String input) {
+        // when
+        String[] result = calculatorParser.parseInput(input);
+
+        // then
+        assertThat(result).containsExactly("");
+
+    }
+    @Test
+    @DisplayName("잘못된 커스텀 구분자 형식")
+    void parseInvalidCustomDelimiter() {
+        assertThrows(IllegalArgumentException.class, () ->
+                calculatorParser.parseInput("//")
+        );
+    }
+}
